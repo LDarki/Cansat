cef89ff801c3b437369d78c2f6afb533c161bc36
LDarki <51938450+LDarki@users.noreply.github.com>
2023-05-15 22:05:37 -0300

    Add video loading icon

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
-window.addEventListener('load', () => {
-    document.querySelector('#presentation').muted = false;
+const video = document.getElementById("presentation");
+const loadingIcon = document.getElementById("video-loading");
+
+video.addEventListener("canplaythrough", function() {
+    loadingIcon.style.display = "none";
+    video.style.display = "block";
+    video.play();  
 });
