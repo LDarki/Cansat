d67726cac3914ba5931995cfd8a2428cd5b3691d
LDarki <51938450+LDarki@users.noreply.github.com>
2023-05-15 22:51:12 -0300

    Fix phone UI + Add sponsors

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,16 @@
-const video = document.getElementById("presentation");
-const loadingIcon = document.getElementById("video-loading");
+(() => {
+    const video = document.getElementById("presentation");
+    const loadingIcon = document.getElementById("video-loading");
 
-video.addEventListener("canplaythrough", function() {
-    loadingIcon.style.display = "none";
-    video.style.display = "block";
-    video.play();  
-});
+    video.addEventListener("canplaythrough", function() {
+        loadingIcon.style.display = "none";
+        video.style.display = "block";
+    });
+
+    const btn = document.querySelector("#burger-toggle");
+    const menu = document.querySelector("#phone-menu");
+
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+})();
